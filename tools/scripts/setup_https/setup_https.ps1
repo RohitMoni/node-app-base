1c8c67c1ca96cca9ff05ed7d826bbe917a812d85
Rohit Moni <rohitmdxb@gmail.com>
2019-04-30 20:50:19 -0400

    Added tool to easily create a self-signed cert for testing + VSCode task to run it

---
+++ tools/scripts/setup_https/setup_https.ps1
@@ -0,0 +1,2 @@
+$ShellScript = Join-Path $PSScriptRoot setup_https.sh
+sh $ShellScript
---
+++ tools/scripts/setup_https/setup_https.sh
@@ -0,0 +1,10 @@
+echo "test"
+
+openssl req \
+    -nodes \
+    -new \
+    -x509 \
+    -subj "//C=US/ST=Rapture/L=Nowhere/O=Dis/CN=localhost" \
+    -keyout encryption/server.key \
+    -out encryption/server.cert
+
